ed53690542895eed50cb69f733882a7f0d81a431
Eldritch <83547364+Sevhena@users.noreply.github.com>
2023-07-03 17:20:16 -0400

    Implement the getComputerChoice() function

---
+++ script.js
@@ -0,0 +1,38 @@
+/*ROCK PAPER SCISSOR GAME
+ *
+ * Objective: A game of rock paper scissors played over 5 rounds.
+ * A single user plays against the "computer".
+ * Whoever has the points points after 5 rounds wins.
+ * 
+ * Pseudocode: 
+ * The computer is the first to make its choice which is stored until the user responds.
+ * As with the traditional game, 
+ * choosing the same move is a tie,
+ * rock winds over scissor,
+ * scissor wins over paper,
+ * paper wins over rock.
+ * 
+ * The result of the round is announced to the user through an output message and also
+ * stored and the next round begins until all 5 rounds are passed.
+ * 
+ * After all 5 rounds, the winner is announced or a tie is declared based on #points.
+*/
+
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+
+    console.log(choice)
+
+    if (choice === 0) {
+        return "ROCK"
+    } 
+    else if (choice === 1) {
+        return "PAPER"
+    }
+    else {
+        return "SCISSOR"
+    }
+}
+
+getComputerChoice()
+
